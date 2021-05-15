069ed34d5d0db83eecab015501ba4f2303dc5b72
onxmoreplz <hamdoyoung95@gmail.com>
2021-05-15 14:29:57 +0900

    [Add] Make Refresh Button

--- mask/app/src/main/java/com/example/masksearch/MainActivity.java
+++ mask/app/src/main/java/com/example/masksearch/MainActivity.java
@@ -10,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                // Refresh 버튼 클릭 이벤트 처리
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
