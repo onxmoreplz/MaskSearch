633b133e7d0181834f0d45bf9a27b25ab91749e0
onxmoreplz <hamdoyoung95@gmail.com>
2021-05-23 15:14:32 +0900

    [Refac] change method to get current location Using Hilt with ViiewModel
    add Method to get Location Permission

--- mask/app/src/main/java/com/example/masksearch/MainViewModel.kt
+++ mask/app/src/main/java/com/example/masksearch/MainViewModel.kt
@@ -1,34 +1,49 @@
 package com.example.masksearch
 
 import MaskService
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.masksearch.model.Store
+import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainViewModel @ViewModelInject constructor(
-    private val service: MaskService
-): ViewModel() {
-    val itemLivaData =  MutableLiveData<List<Store>>()
-    val loadingLiveData =  MutableLiveData<Boolean>()
+    private val service: MaskService,
+    private val fusedLocationProviderClient: FusedLocationProviderClient
+) : ViewModel() {
+    val itemLivaData = MutableLiveData<List<Store>>()
+    val loadingLiveData = MutableLiveData<Boolean>()
 
     init {
+
         fetchStoreInfo()
     }
 
+    @SuppressLint("MissingPermission")
     fun fetchStoreInfo() {
         loadingLiveData.value = true //  로딩 시작
 
-        viewModelScope.launch {
-            val storeInfo = service.fetchStoreInfo(37.1111, 128.2222)
-            itemLivaData.value = storeInfo.stores
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener { location ->
+                viewModelScope.launch {
+                    val storeInfo = service.fetchStoreInfo(location.latitude, location.longitude)
+                    itemLivaData.value = storeInfo.stores.filter { store ->
+                        store.remain_stat != null
+                    }
 
-            loadingLiveData.value = false // 로딩 종료
-        }
+                    loadingLiveData.value = false // 로딩 종료
+                }
+            }
+            .addOnFailureListener {exception ->
+                loadingLiveData.value = false // 로딩 종료
+            }
+
     }
 
 
---
+++ mask/app/src/main/java/com/example/masksearch/di/module/LocationModule.kt
@@ -0,0 +1,19 @@
+package com.example.masksearch.di.module
+
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object LocationModule {
+    @Provides
+    fun provideFusedLocationProviderClient(@ApplicationContext context: Context): FusedLocationProviderClient? {
+        return LocationServices.getFusedLocationProviderClient(context)
+    }
+}
